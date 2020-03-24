e84c06fbc930054462dc74a7617f72224f31c23b
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-24 19:43:48 +0100

    Fill friends list with static content

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,11 +1,13 @@
 <h3 class="font-bold text-lg mb-4">Friends</h3>
 <ul>
-    <li>
-        <div class="flex items-center">
-            <img src="https://i.pravatar.cc/40"
-                alt=""
-                class="rounded-full mr-2">
-            Jon Doe
-        </div>
-    </li>
+    @foreach (range(1,8) as $index)
+        <li class="mb-4">
+            <div class="flex items-center text-sm">
+                <img src="https://i.pravatar.cc/40"
+                    alt=""
+                    class="rounded-full mr-2">
+                Jon Doe
+            </div>
+        </li>
+    @endforeach
 </ul>
