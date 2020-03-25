439a8c132dbec90a78af70a668b5f48f03fabb5e
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-25 20:34:48 +0100

    Design the timeline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,34 @@
 @section('content')
     <div class="lg:flex">
         <div class="lg:w-1/6">
-            @include('_sidebar-links')
+            @include ('_sidebar-links')
         </div>
-        <div class="lg:flex-1 lg:mx-10">
-            <div class="border border-blue-400 rounded-lg py-6 px-8">
-                <form action="">
-                    <textarea name="body"
-                        class="w-full"
-                        placeholder="What's up doc?"></textarea>
-                    <hr class="my-4">
 
-                    <footer class="flex justify-between">
-                        <img src="https://i.pravatar.cc/40"
+        <div class="lg:flex-1 lg:mx-10">
+            @include ('_publish-tweet-panel')
+
+            <div class="border border-gray-300 rounded-lg">
+                <div class="flex p-4">
+                    <div class="mr-2 flex-shrink-0">
+                        <img src="https://i.pravatar.cc/50"
                             alt=""
                             class="rounded-full mr-2">
-                        <button type="submit"
-                            class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">
-                            Tweet-a-roo
-                        </button>
-                    </footer>
-                </form>
+                    </div>
+                    <div>
+                        <h5 class="text-bold mb-4">Jon Doe</h5>
+                        <p class="text-small">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                             elit. Tempore ratione vitae numquam, ipsa
+                              reprehenderit sapiente unde qui consequuntur cum.
+                               Vel.
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div>Timeline</div>
         </div>
+
         <div class="lg:w-1/6">
-            @include('_friends-list')
+            @include ('_friends-list')
         </div>
     </div>
 @endsection
