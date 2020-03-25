5d29feb0623bebe7aacb4479bb6cdc8be416532c
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-25 20:36:43 +0100

    Extract tweet design to a partial

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,16 @@
+<div class="flex p-4">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50"
+            alt=""
+            class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="text-bold mb-4">Jon Doe</h5>
+        <p class="text-small">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Tempore ratione vitae numquam, ipsa
+                reprehenderit sapiente unde qui consequuntur cum.
+                Vel.
+        </p>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,22 +10,10 @@
             @include ('_publish-tweet-panel')
 
             <div class="border border-gray-300 rounded-lg">
-                <div class="flex p-4">
-                    <div class="mr-2 flex-shrink-0">
-                        <img src="https://i.pravatar.cc/50"
-                            alt=""
-                            class="rounded-full mr-2">
-                    </div>
-                    <div>
-                        <h5 class="text-bold mb-4">Jon Doe</h5>
-                        <p class="text-small">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                             elit. Tempore ratione vitae numquam, ipsa
-                              reprehenderit sapiente unde qui consequuntur cum.
-                               Vel.
-                        </p>
-                    </div>
-                </div>
+                @include ('_tweet')
+                @include ('_tweet')
+                @include ('_tweet')
+                @include ('_tweet')
             </div>
         </div>
 
