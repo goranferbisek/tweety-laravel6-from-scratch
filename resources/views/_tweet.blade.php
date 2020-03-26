dd12d4eda5f4bef058f4efeb6391c2657c8ec063
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-26 17:19:27 +0100

    Always get same avatar photo

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,6 +1,6 @@
 <div class="flex p-4 border-b border-b-grey-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50"
+        <img src="https://i.pravatar.cc/50?u={{ $tweet->user->email }}"
             alt=""
             class="rounded-full mr-2">
     </div>
