3a7a3c469660c163d4c06192f1c88255327f9144
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-26 18:05:01 +0100

    Make a working tweet input

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,16 +1,26 @@
 <div class="border border-blue-400 rounded-lg py-6 px-8 mb-8">
-    <form action="">
-        <textarea name="body"
+    <form method="POST" action="/tweets">
+        @csrf
+
+        <textarea
+            name="body"
             class="w-full"
-            placeholder="What's up doc?"></textarea>
+            placeholder="What's up doc?"
+            required
+        ></textarea>
+
         <hr class="my-4">
 
         <footer class="flex justify-between">
-            <img src="{{ auth()->user()->avatar }}"
-                alt=""
-                class="rounded-full mr-2">
-            <button type="submit"
-                class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">
+            <img
+                src="{{ auth()->user()->avatar }}"
+                alt="avatar photo"
+                class="rounded-full mr-2"
+            >
+            <button
+                type="submit"
+                class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white"
+            >
                 Tweet-a-roo
             </button>
         </footer>
