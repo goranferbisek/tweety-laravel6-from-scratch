200764dd3045aa5d58fadd6e32c23e73c08db7fd
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-03-27 21:43:53 +0100

    Define follow relationships

--- app/User.php
+++ app/User.php
@@ -47,4 +47,18 @@ class User extends Authenticatable
     {
         return Tweet::where('user_id', $this->id)->latest()->get();
     }
+
+    public function follow(User $user) {
+        return $this->follows()->save($user);
+    }
+
+    public function follows()
+    {
+
+        return $this->belongsToMany(
+            User::class,
+            'follows', // expects user_user table because of naming convention
+            'user_id',
+            'following_user_id');
+    }
 }
