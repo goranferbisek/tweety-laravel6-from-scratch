507d8d586c957744c4a752748fb9f83000c51fda
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-05 19:13:46 +0200

    Link to my profile page

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -50,7 +50,7 @@
     <li>
         <a
             class="font-bold text-lg mb-4 block"
-            href="#"
+            href="{{ route('profile', auth()->user()) }}"
         >
             Profile
         </a>
