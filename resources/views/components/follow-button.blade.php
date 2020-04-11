4857458e0029a741dcab38b3b4ff8729bc087790
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-11 18:29:12 +0200

    Extract getting current user to helper functions

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,9 +1,9 @@
-@if(auth()->user()->isNot($user))
+@if(current_user()->isNot($user))
     <form method="POST" action="/profiles/{{ $user->name }}/follow">
         @csrf
         <button type="submit"
             class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
-            {{ auth()->user()->following($user) ? 'Unfollow me' : 'Follow me' }}
+            {{ current_user()->following($user) ? 'Unfollow me' : 'Follow me' }}
         </a>
     </form>
 @endif
