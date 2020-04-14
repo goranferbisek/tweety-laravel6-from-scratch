fcbd7a4e769c7bf9c05f9e22f7abfa0c7c94f5b6
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-14 16:58:52 +0200

    Fix blank content for fresh profiles

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,6 +1,6 @@
 <h3 class="font-bold text-lg mb-4">Following</h3>
 <ul>
-    @foreach (auth()->user()->follows as $user)
+    @forelse (auth()->user()->follows as $user)
         <li class="mb-4">
             <a href="{{ route('profile', $user) }}"
                class="flex items-center text-sm"
@@ -14,5 +14,7 @@
                 {{ $user->name }}
             </a>
         </li>
-    @endforeach
+    @empty
+        <li>No friends yet.</li>
+    @endforelse
 </ul>
