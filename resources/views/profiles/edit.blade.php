c43574d396842f5ab23f458f99f1065a4661d039
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-16 21:12:52 +0200

    Fix minor error

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -91,7 +91,7 @@
                 required
             >
 
-            @error('name')
+            @error('email')
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
             @enderror
         </div>
