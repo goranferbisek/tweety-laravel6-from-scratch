158d39fed461fab25fcfcf9bc76ff273709df63e
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-17 23:10:20 +0200

    Remove unused links in the navigation

--- resources/views/_sidebar-links.blade.php
+++ resources/views/_sidebar-links.blade.php
@@ -18,38 +18,6 @@
     <li>
         <a
             class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Notifications
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Messages
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Bookmarks
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
-            href="#"
-        >
-            Lists
-        </a>
-    </li>
-    <li>
-        <a
-            class="font-bold text-lg mb-4 block"
             href="{{ route('profile', auth()->user()) }}"
         >
             Profile
