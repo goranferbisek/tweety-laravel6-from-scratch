23fd8940c143d350b6854453443d7eb46127d5ff
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-04-18 11:38:35 +0200

    Make likes model with a database migration

--- app/Tweet.php
+++ app/Tweet.php
@@ -12,4 +12,23 @@ class Tweet extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function like($user = null, $liked = true)
+    {
+        $this->likes->updateOrCreate([
+            'user_id' => $user ? $user->id : auth()->id()
+        ], [
+            'liked'=> $liked
+        ]);
+    }
+
+    public function dislike()
+    {
+        return $this->like(false);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
