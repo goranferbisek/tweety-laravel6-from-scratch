fd53de3128f4d4dc9cc10cb624f43c3c2990e7e9
Goran Ferbišek <goran.ferbisek@gmail.com>
2020-07-09 20:04:38 +0200

    Get the file object on file drag and drop

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
@@ -6,27 +6,27 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+let dropZone = document.querySelector('.drop-zone');
+let imageInput = document.querySelector('.tweet-image');
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+dropZone.addEventListener('dragover', e => {
+    e.preventDefault();
+    dropZone.classList.add('font-bold');
+});
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+dropZone.addEventListener('drop', e => {
+    e.preventDefault();
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+    if (e.dataTransfer.files.length) {
+        imageInput.files = e.dataTransfer.files;
+        console.log(imageInput.files[0]);
 
-const app = new Vue({
-    el: '#app',
+    }
+});
+
+['dragleave', 'dragend'].forEach(type => {
+    dropZone.addEventListener(type, e => {
+        dropZone.classList.remove('font-bold');
+    });
 });
